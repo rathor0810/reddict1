136c132cf577d26f4a70373a33db8ddfa9105afc
rathor0810 <shivamrathor23215@gmail.com>
2024-02-18 09:22:42 +0530

    loading indicator if data is taking time

--- lib/feature/auth/controller/auth_controller.dart
+++ lib/feature/auth/controller/auth_controller.dart
@@ -2,22 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddict1/core/utils.dart';
 import 'package:reddict1/feature/auth/repository/auth_repository.dart';
+import 'package:reddict1/models/user_model.dart';
 
-final authControllerProvider = Provider(
-    (ref) => AuthController(authRepository: ref.read(authRepositoryProvider)));
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
-class AuthController {
+final authControllerProvider = StateNotifierProvider<AuthController,bool>((ref) =>
+    AuthController(authRepository: ref.watch(authRepositoryProvider), ref: ref));
+
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
+  final Ref _ref;
 
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref,
+        super(false);
 
   void signInWithGoogle(BuildContext context) async {
+    state = true;
     final user = await _authRepository.signInWithGoogle();
+    state = false;
     user.fold(
-        (l) => showSnackBar(
-              context,
-            l.message),
-        (r) => null);
+        (l) => showSnackBar(context, l.message),
+        (UserModel) =>
+            _ref.read(userProvider.notifier).update((state) => UserModel));
   }
 }
--- lib/feature/auth/screens/login_screen.dart
+++ lib/feature/auth/screens/login_screen.dart
@@ -1,55 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddict1/core/common/loader.dart';
 import 'package:reddict1/core/common/sign_in_button.dart';
 import 'package:reddict1/core/constants/constants.dart';
+import 'package:reddict1/feature/auth/controller/auth_controller.dart';
 
-
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Image.asset(
-              Constants.logopath,
-              height: 40,
+      appBar: AppBar(
+        title: Center(
+          child: Image.asset(
+            Constants.logopath,
+            height: 40,
+          ),
+        ),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'skip',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ))
+        ],
+      ),
+      body: isLoading ? const Loader(): Column (
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          const Center(
+            child: Text(
+              'Dive Into Anything',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                  letterSpacing: 0.5),
             ),
           ),
-          actions: [
-            TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'skip',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ))
-          ],
-        ),
-        body:  Column(
-          children: [
-            const SizedBox(
-              height: 30,
-              
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              Constants.loinEmothepath,
+              height: 300,
             ),
-            const Center(child: Text('Dive Into Anything', style: TextStyle(fontWeight: FontWeight.bold,fontSize:28,letterSpacing: 0.5),
-            ),
-            ),
-            const SizedBox(
-              height: 30,
-              
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(Constants.loinEmothepath, height: 300,
-              ),
-            ),
-             const SizedBox(height: 30,),
-             const SignInButton(),
-          ],
-          
-          
-        ),
-        
-        );
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const SignInButton(),
+        ],
+      ),
+    );
   }
 }
