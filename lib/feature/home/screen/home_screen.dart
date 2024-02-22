4a1a7dedfd8b185a6c5feb653925560d5b4bc7a0
rathor0810 <shivamrathor23215@gmail.com>
2024-02-22 10:10:55 +0530

    create community added

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddict1/feature/auth/controller/auth_controller.dart';
+import 'package:reddict1/feature/home/drawers/community_list_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -12,20 +17,25 @@ class HomeScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Home'),
         centerTitle: false,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () => displayDrawer(context),
+            );
+          }
         ),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)
-          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
           IconButton(
             icon: CircleAvatar(
               backgroundImage: NetworkImage(user!.profilePic),
-            ), onPressed: () {  },
+            ),
+            onPressed: () {},
           ),
         ],
       ),
+      drawer: const CommunityListDrawer(),
     );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:reddict1/feature/auth/screens/login_screen.dart';
+import 'package:reddict1/feature/community/screens/create_community_screen.dart';
 import 'package:reddict1/feature/home/screen/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute =
     RouteMap(routes: {'/': (_) => const MaterialPage(child: LoginScreen())});
 final loggedInRoute =
-    RouteMap(routes: {'/': (_) => const MaterialPage(child: HomeScreen())});
+    RouteMap(routes: {'/': (_) => const MaterialPage(child: HomeScreen()),
+    '/create-community': (_) => const MaterialPage(child: CreateCommunityScreen()),});
